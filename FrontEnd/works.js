18cbdf50e1ac1910678a7d55071c783c04a1a34c
Lucas Couchy <lukcouchy@gmail.com>
2023-03-12 15:03:30 +0100

    Etape 1 terminée

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,34 +1,54 @@
+
+// **************** FETCH ********************
+
 const reponse = await fetch ('http://localhost:5678/api/works');
 let works = await reponse.json();
-
 console.log(works);
 
+// **************** INIT ********************
 
 const gallery = document.querySelector('.gallery');
-
-
 let categories = document.querySelector('#categories');
 let tabbtn = Array.from(categories.children);
 
+const tous = document.querySelector('.b0');
+const objet = document.querySelector('.b1');
+const appartements = document.querySelector('.b2');
+const hôtels = document.querySelector('.b3');
+
+let category_active = 0;
+
+
+// **************** FUNCTIONS ********************
 function removeActif() {
     for (let i = 0; i < tabbtn.length; i++) {
         tabbtn[i].classList.remove('actif');
     }
 }
 
-function deleteWorks() {
-    for (let i = 0; i < displayed_works; i++) {
-        const figure = document.querySelector(`.gallery .fig_${i}`);
-        gallery.remove(figure);
+function filter () {
+    // on display none les élements qui ne correspondent pas
+    for (let i = 0; i < works.length; i++) {
+        
+        const figure = document.querySelector(`.gallery figure.fig_${i}`);
+        if (figure.classList[1] !== `cat_${category_active}`) {
+            figure.style.display = 'none';
+        }
+
+        else {
+            figure.style.display = 'block';   
+        }
     }
-    displayed_works = 0;
 }
 
 
-function displayWorks(i) {
+// **************** MAIN ********************
+
+// on affiche les projets dans la gallerie
+for (let i = 0; i < works.length; i++) {
     let tab = [];
     tab[i] = document.createElement('figure');
-    tab[i].classList.add(`fig_${i}`,`cat${works[i].categoryId}`);
+    tab[i].classList.add(`fig_${i}`,`cat_${works[i].categoryId}`);
     gallery.append(tab[i]);
     
     const figure = document.querySelector(`.gallery .fig_${i}`);
@@ -39,75 +59,40 @@ function displayWorks(i) {
     tab[i] = document.createElement('figcaption');
     tab[i].innerHTML = works[i].title;
     figure.append(tab[i]);
-
-    displayed_works++;
-
 }
 
-const tous = document.querySelector('.b0');
-const objet = document.querySelector('.b1');
-const appartements = document.querySelector('.b2');
-const hôtels = document.querySelector('.b3');
-
-let category_active = 0;
-let displayed_works = works.length;
-
-function filter_works() {
-    for (let i = 0; i < works.length; i++) {
-        
-
-        if (category_active == 0) {
-            displayWorks(i);
-            
-        }
-        
-        else if (category_active == 1) {
-            if (works[i].categoryId == category_active) {
-                displayWorks(i);
-                
-            }
-        }
-        else if (category_active == 2) {
-            if (works[i].categoryId == category_active) {
-                displayWorks(i);
-            }
-        }
-        else if (category_active == 3) {
-            if (works[i].categoryId == category_active) {
-                displayWorks(i);
-            }
-        }
-    }
-}
-
-filter_works();
+// on écoute l'évènement click sur les boutons
 
 objet.addEventListener ('click', function() {
-    console.log('objet');
     category_active = 1;
-    deleteWorks();
-    filter_works();
     removeActif();
     tabbtn[category_active].classList.add('actif');
+    filter();
 })
+
 appartements.addEventListener ('click', function() {
     category_active = 2;
-    deleteWorks();
-    filter_works();
     removeActif();
     tabbtn[category_active].classList.add('actif');
+    filter();
 })
+
 hôtels.addEventListener ('click', function() {
     category_active = 3;
-    deleteWorks();
-    filter_works();
     removeActif();
     tabbtn[category_active].classList.add('actif');
+    filter();
 })
+
 tous.addEventListener ('click', function() {
     category_active = 0;
-    deleteWorks();
-    filter_works();
     removeActif();
     tabbtn[category_active].classList.add('actif');
+
+    for (let i = 0; i < works.length; i++) {
+        const figure = document.querySelector(`.gallery figure.fig_${i}`);
+        figure.style.display = 'block'; 
+    }
 })
+
+
